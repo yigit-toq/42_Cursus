ba24ae302186da43a08a76b743949f323bce155c
Yiğit Top <yigit.toq@gmail.com>
2024-12-24 17:17:02 +0300

    24/12 School Part1

--- Cub3D/assets/sounds/audio.c
+++ Cub3D/assets/sounds/audio.c
@@ -1,45 +1,54 @@
+#include "cub3d_bonus.h"
 #include <SDL2/SDL.h>
-#include <stdio.h>
 
-Uint8			*audio_buf;
-
-Uint32			audio_len;
-
-SDL_AudioSpec	spec;
-
-void play_audio(const char *filename)
+typedef struct s_audio
 {
-	if (SDL_Init(SDL_INIT_AUDIO) < 0)
+	Uint8	*audio_pos;
+	Uint32	audio_len;
+}			t_audio;
+
+static void	audio_callback(void *userdata, Uint8 *stream, int len)
+{
+	t_audio	*audio;
+
+	audio = (t_audio *)userdata;
+	if (audio->audio_len == 0)
 	{
-		fprintf(stderr, "SDL_InitError: %s\n", SDL_GetError());
-		return;
+		return ;
 	}
-	if (SDL_LoadWAV(filename, &spec, &audio_buf, &audio_len) == NULL)
-	{
-		fprintf(stderr, "SDL_LoadWAV Error: %s\n", SDL_GetError());
-		SDL_Quit();
-		return;
-	}
-	if (SDL_OpenAudio(&spec, NULL) < 0)
-	{
-		fprintf(stderr, "SDL_OpenAudio Error: %s\n", SDL_GetError());
-		SDL_FreeWAV(audio_buf);
-		SDL_Quit();
-		return;
-	}
-	SDL_PauseAudio(0);
-
-	SDL_Delay((audio_len / spec.freq) * 1000);
-
-	SDL_CloseAudio();
-
-	SDL_FreeWAV(audio_buf);
-
-	SDL_Quit();
+	if (len > audio->audio_len)
+		len = audio->audio_len;
+	SDL_memcpy(stream, audio->audio_pos, len);
+	audio->audio_pos += len;
+	audio->audio_len -= len;
 }
 
-int	main()
+int	play_sound(char *file_path)
 {
-	play_audio("../../../../backup/bg_music.wav");
-	return (0);
+	Uint8 			*wav_buffer;
+	Uint32 			wav_length;
+	SDL_AudioSpec	wav_spec;
+	t_audio 		audio;
+
+	if (SDL_Init(SDL_INIT_AUDIO) < 0)
+		error_controller(SDL_GetError(), NULL);
+	if (SDL_LoadWAV(file_path, &wav_spec, &wav_buffer, &wav_length) == NULL)
+	{
+		SDL_Quit();
+		error_controller(SDL_GetError(), NULL);
+	}
+	audio.audio_pos = wav_buffer;
+	audio.audio_len = wav_length;
+	wav_spec.callback = audio_callback;
+	wav_spec.userdata = &audio;
+	if (SDL_OpenAudio(&wav_spec, NULL) < 0),
+	{
+		SDL_FreeWAV(wav_buffer);
+		SDL_Quit();
+		error_controller(SDL_GetError(), NULL);
+	}
+	SDL_PauseAudio(0);
+	while (audio.audio_len > 0)
+		SDL_Delay(100);
+    return (SDL_CloseAudio(), SDL_FreeWAV(wav_buffer), SDL_Quit(), SUCCESS);
 }
