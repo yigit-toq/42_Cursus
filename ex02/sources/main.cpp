e0015a40974e9c39787418a68fcc2f0dbd740b58
Yiğit Top <yigit.toq@gmail.com>
2025-01-22 16:29:40 +0300

    main update

--- ex02/sources/main.cpp
+++ ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ytop <ytop@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 14:16:47 by ytop              #+#    #+#             */
-/*   Updated: 2025/01/16 14:33:55 by ytop             ###   ########.fr       */
+/*   Updated: 2025/01/22 16:26:27 by ytop             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,17 @@ int	main()
 	std::cout << b << std::endl;
 
 	std::cout << Fixed::max( a, b ) << std::endl;
+
+	Fixed	n1(45.10f);
+	Fixed	n2(42);
+
+	std::cout << "n1 = " << n1 << std::endl;
+	std::cout << "n2 = " << n2 << std::endl;
+
+	std::cout << "n1 + n2 = " << n1 + n2 << std::endl;
+	std::cout << "n1 - n2 = " << n1 - n2 << std::endl;
+	std::cout << "n1 * n2 = " << n1 * n2 << std::endl;
+	std::cout << "n1 / n2 = " << n1 / n2 << std::endl;
 
 	return (0);
 }
