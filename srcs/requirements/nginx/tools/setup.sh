7110e2097f559b6df9eebb61b70fd09794dd5ef5
yigit-toq <yigit.toq@gmail.com>
2025-11-04 14:56:52 +0300

    fixed setup.sh

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -8,11 +8,11 @@ mkdir -p /etc/nginx/ssl
 if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
 	echo "[i] NGINX: Kendi imzaladığımız SSL sertifikası oluşturuluyor..."
 	
-	openssl req -x509 -nodes -days 365 -newkey rsa:2048				\
-		-keyout	/etc/nginx/ssl/nginx.key							\
-		-out	/etc/nginx/ssl/nginx.crt							\
-		-subj "	/C=TR/ST=Kocaeli/L=Gebze/O=42School/CN=$DOMAIN_NAME"
-else
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+		-keyout /etc/nginx/ssl/nginx.key \
+		-out /etc/nginx/ssl/nginx.crt \
+		-subj "/C=TR/ST=Kocaeli/L=Gebze/O=42School/CN=$DOMAIN_NAME"
+	else
 	echo "[i] NGINX: SSL sertifikası zaten mevcut."
 fi
 
